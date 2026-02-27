3e8de6461273e5768dd4148169ae108503b061a3
vishnunaniinfo <vishnunani2129@gmail.com>
2026-02-27 12:54:54 +0530

    Add Apollo website tracker script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next"
+import Script from "next/script"
 
 import "./globals.css"
 
@@ -34,7 +35,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-sans antialiased">{children}</body>
+      <body className="font-sans antialiased">
+        {children}
+        <Script id="apollo-tracker" strategy="afterInteractive">
+          {`(function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,o.onload=function(){window.trackingFunctions.onLoad({appId:"69a1402c0f6e4e000d6aec3c"})},document.head.appendChild(o)})();`}
+        </Script>
+      </body>
     </html>
   )
 }
